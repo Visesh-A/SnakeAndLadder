486d82a147b76a98efae3782a95fc72851dfa71e
“Visesh-A” <viseshajl@gmail.com>
2023-03-07 22:47:18 +0530

    [visesh]Add:Successful

--- SnakeAndLadder/Play.cs
+++ SnakeAndLadder/Play.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 namespace SnakeAndLader
 {
-    public class Game
+    public class Play
     {
         int PlayerPosition = 0;
-        Random random = new Random();
-        public void DieRoll()
-        {
-            int die = random.Next(1, 7);
-            Console.WriteLine(die);
-        }
     }
 }
